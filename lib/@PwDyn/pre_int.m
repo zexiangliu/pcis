c255a4d8945774823abca3624a9d1fba3ad065d2
zexiangliu <zexiang@umich.edu>
2018-11-04 15:13:41 -0500

    add parallel support in pre_int.m

--- lib/@PwDyn/pre_int.m
+++ lib/@PwDyn/pre_int.m
@@ -1,4 +1,4 @@
-function [preSets] = pre_int(pwd1, pwd2, X, rho, regions, dyns_id)
+function [preSets] = pre_int(pwd1, pwd2, X, rho, regions, dyns_id, isParallel)
 
 	% Usage:
 	%	preSets = pre_int(pwd1 , pwd2, X , rho)
@@ -31,24 +31,55 @@ function [preSets] = pre_int(pwd1, pwd2, X, rho, regions, dyns_id)
         num_reg = length(regions);
     end
     
+    if(nargin <= 6)
+        isParallel = false;
+    end
+    
 	preSets = PolyUnion;
     MaxNum = 20;
-	for i=1:num_reg
-        dyn_id1 = dyns_id{i}(1);
-        dyn_id2 = dyns_id{i}(2);
-	    new_poly = IntersectPolyUnion(regions{i},...
-            pwd1.dyn_list{dyn_id1}.preIntUnion(...
-            pwd2.dyn_list{dyn_id2},X, rho));
-        
-% 		
-        for j = 1:new_poly.Num
-            preSets.add(new_poly.Set(j));
+    
+    if(isParallel)
+    
+        new_poly = cell(num_reg,1);
+        parfor i=1:num_reg
+            dyn_id1 = dyns_id{i}(1);
+            dyn_id2 = dyns_id{i}(2);
+            new_poly{i} = IntersectPolyUnion(regions{i},...
+                pwd1.dyn_list{dyn_id1}.preIntUnion(...
+                pwd2.dyn_list{dyn_id2},X, rho));
+        end
+
+        for i = 1:num_reg
+            for j = 1:new_poly{i}.Num
+                preSets.add(new_poly{i}.Set(j));
+            end
         end
         % reduce if too many polyhedron
         if(preSets.Num >= MaxNum)
             preSets.reduce;
-            MaxNum = max(MaxNum,2*preSets.Num);
         end
-	end
-
+    else
+        for i=1:num_reg
+            dyn_id1 = dyns_id{i}(1);
+            dyn_id2 = dyns_id{i}(2);
+            new_poly = IntersectPolyUnion(regions{i},...
+                pwd1.dyn_list{dyn_id1}.preIntUnion(...
+                pwd2.dyn_list{dyn_id2},X, rho));
+            
+            for j = 1:new_poly.Num
+                preSets.add(new_poly.Set(j));
+            end
+            
+%             % reduce if too many polyhedron
+%             if(preSets.Num >= MaxNum)
+%                 preSets.reduce;
+%                 MaxNum = max(MaxNum,preSets.Num*2);
+%             end
+        end
+        
+        % reduce if too many polyhedron
+        if(preSets.Num >= MaxNum)
+            preSets.reduce;
+        end
+    end
 end
