791def2d2b29927ae65ecc77a786a2074c89844b
ysahin <ysahin@ysahins-MacBook-Pro.local>
2018-11-10 15:44:31 -0500

    ignore these changes

--- examples/takeover_expand.m
+++ examples/takeover_expand.m
@@ -23,16 +23,23 @@ con = constants_tri();
 [dyn_a , dyn_c] = get_takeover_pwd();
 
 %% Create Safe Set and Small Invariant Set
-
-X1 = Polyhedron('UB', [con.v_max;   con.y_max;      con.h_max;      Inf],...
+X1 = Polyhedron('UB', [con.v_max;   con.y_max;       Inf;      Inf],...
                 'LB', [con.v_min;   con.y_min;      con.h_min;     -Inf]);
 X2 = Polyhedron('UB', [con.v_max;   con.y_max;      con.h_max;     Inf],...
                 'LB', [con.v_min;   -con.y_min;     -con.h_max;    -Inf]);
 X3 = Polyhedron('UB', [con.v_max;   con.y_max;      -con.h_min;    Inf],...
-                'LB', [con.v_min;   con.y_min;      -con.h_max;    -Inf]);
+                'LB', [con.v_min;   con.y_min;      - Inf;    -Inf]);
+
+
+% X1 = Polyhedron('UB', [con.v_max;   con.y_max;       Inf;       con.vL_max],...
+%                 'LB', [con.v_min;   -con.y_min;      con.h_min;       con.vL_min]);
+% X2 = Polyhedron('UB', [con.v_max;   con.y_max;      Inf;        con.vL_max],...
+%                 'LB', [con.v_min;   con.y_min;     -Inf;       con.vL_min]);
+% X3 = Polyhedron('UB', [con.v_max;   con.y_max;      -con.h_min;        con.vL_max],...
+%                 'LB', [con.v_min;   -con.y_min;      -Inf;       con.vL_min]);
 
 % Safe set 
-S = PolyUnion([X1 X2 X3]); 
+S = PolyUnion([X1 X3]); 
 % figure;clf;hold on
 % for s=1:S.Num
 %     plot(S.Set(s).projection([2 3]));
@@ -46,7 +53,7 @@ C = X2;
 
 % reach
 rhoPre = 1e-6;
-Xr = expand(dyn_c, S, C, rhoPre,'plot_stuff','debug','max_iter',10);
+Xr = expand(dyn_c, S, C, rhoPre,'plot_stuff','debug','max_iter',100);
 
 %% Plotting
 figure;
