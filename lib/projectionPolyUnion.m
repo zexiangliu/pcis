35663392992bd20aaf584e71efe4a0fea76479dc
zexiangliu <zexiang@umich.edu>
2018-11-11 16:05:07 -0500

    add minHRep in projectionPolyUnion and IntersectPolyUnion

--- lib/projectionPolyUnion.m
+++ lib/projectionPolyUnion.m
@@ -1,7 +1,7 @@
 function proj = projectionPolyUnion(P,dims)
     proj = PolyUnion;
     for i = 1:P.Num
-        proj.add(P.Set(i).projection(dims));
+        proj.add(P.Set(i).projection(dims).minHRep);
     end
 
 end
